1b4e2f35d2912203739b23d2923715f1e0de729e
zbass90 <smilejkh90@gmail.com>
2023-11-26 00:39:43 +0900

    spring-boot config

---
+++ src/main/java/com/basic/memo/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.basic.memo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().authenticated()
+//                )
+        ;
+        return http.build();
+    }
+
+}
